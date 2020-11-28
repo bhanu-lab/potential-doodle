527ac02d792f6efb17bc206c250e6a6b10418f71
chota-bheem <bhanuputtareddy@gmail.com>
2020-11-28 18:13:06 +0530

    added to read options from command line

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "potential-doodle/events"
+import (
+	"fmt"
+	"os"
+	"potential-doodle/events"
+)
 
 func main() {
-	events.DisplayEvents()
+	argsWithoutProg := os.Args[1:]
+
+	if len(argsWithoutProg) == 0 {
+		fmt.Printf("no options are specified to process. check with -h option")
+	}
+
+	if argsWithoutProg[0] == "-d" {
+		events.DisplayEvents()
+	} else if argsWithoutProg[0] == "-h" {
+		fmt.Printf("\t-d\tDisplay next 10 upcoming events \n")
+	}
 }
