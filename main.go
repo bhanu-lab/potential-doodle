478123b27677e7cfbf708c00f5e34a3121e29d4c
chota-bheem <bhanuputtareddy@gmail.com>
2020-11-29 12:37:34 +0530

    modified changes for create event

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"potential-doodle/events"
+
+	"google.golang.org/api/calendar/v3"
 )
 
 func main() {
@@ -17,5 +19,19 @@ func main() {
 		events.DisplayEvents()
 	} else if argsWithoutProg[0] == "-h" {
 		fmt.Printf("\t-d\tDisplay next 10 upcoming events \n")
+	} else if argsWithoutProg[0] == "-c" {
+		duration := 0
+		recurrence := ""
+		startTime := &calendar.EventDateTime{
+			DateTime: "",
+			TimeZone: "",
+		}
+
+		endTime := &calendar.EventDateTime{
+			DateTime: "",
+			TimeZone: "",
+		}
+
+		events.CreateEvent(argsWithoutProg[1], argsWithoutProg[2], startTime, endTime, duration, recurrence)
 	}
 }
